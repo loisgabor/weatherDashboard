a1a61ce0ba65a7096d8571ad1f6722f84e4783d3
Lois Simonca <loisgabor@gmail.com>
2020-10-13 23:09:00 -0400

    fixed UV Index

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
         }
 
         $(".uv-index").text("UV-Index: " + uvIndex);
-        $(".uv-index").attr("class", `btn ${condition}`);
+        $(".uv-index").attr("class", `uv-index btn ${condition}`);
       });
     });
   }
