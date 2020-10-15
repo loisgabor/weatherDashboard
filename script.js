c34fe750692a673c8741832994cea1b670d3f62d
Lois Simonca <loisgabor@gmail.com>
2020-10-14 22:38:53 -0400

    Made past city buttons work

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ $(document).ready(function () {
     event.preventDefault();
     var citySearch = $("#inputCity").val();
     var cityButtonEl = $("<button>");
+
     cityButtonEl.text(citySearch);
     cityButtonEl.attr("id", "button" + citySearch);
     cityButtonEl.attr("class", "btn-light btn-lg btn-block");
+    cityButtonEl.on("click", function () {
+      var text = this.textContent;
+      getCity(text);
+    });
+    cityButtonEl.attr(
+      "style",
+      "margin:30px; width:80%; background-color:rgba(38, 109, 241, 0.979"
+    );
     $(".card-bodyA").append(cityButtonEl);
+
     localStorage.setItem("cityName" + citySearch, citySearch);
     getCity(citySearch);
     $(".forecast").show();
@@ -140,11 +150,6 @@ $(document).ready(function () {
         .text(dayFiveDate, weatherPicFive, tempFfive, humidCityFive)
 
         .append(tempFfive, weatherPicFive, humidCityFive);
-      
-      
     });
-    $("#btn(citySearch").on("click", function (event) {
-        event.preventDefault();
-        
   }
 });
